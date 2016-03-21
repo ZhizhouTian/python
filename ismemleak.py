061f21ad570833e0ca2e85cb163dd654b5062477
Zhizhou Tian <zhizhou.tian@gmail.com>
2016-03-21 17:45:48 +0800

    memory leak: get lost ration's linear regression
    tell people if the data is a memory leak through linear regression
    
    Change-Id: I799ea620ad7411dee52c317437725cf9809f5de6

---
+++ ismemleak.py
@@ -0,0 +1,72 @@
+import os
+import re
+import datetime
+try:
+	import pandas.io.data as pd
+except:
+	if platform.system() == "Linux":
+		os.system("sudo pip install pandas")
+	else:
+		os.system("pip install pandas")
+	import pandas.io.data as pd
+
+try:
+	import statsmodels.formula.api as sm
+except:
+	if platform.system() == "Linux":
+		os.system("sudo pip install statsmodels")
+	else:
+		os.system("pip install statsmodels")
+	import statsmodels.formula.api as sm
+
+try:
+	import matplotlib.pyplot as plt
+except:
+	if platform.system() == "Linux":
+		os.system("sudo pip install matplotlib")
+	else:
+		os.system("pip install matplotlib")
+	import matplotlib.pyplot as plt
+
+class Report(object):
+	losts = []
+
+'''
+Parameters:
+	filename: file path and name include memory lost data
+Return:
+	0: there is not any memory leak 
+	1: there are memory leaks
+'''
+def ismemleak(filename):
+	report = Report()
+	if not os.path.exists(filename):
+		raise Exception("%s does not exists")
+	with open(filename) as filp:
+		line = filp.readline()
+		while line:
+			if "lost_ration" in line:
+				lostvals = re.findall(r"\d+\.\d+", line)
+				report.losts.append(float(lostvals[0])* 1000)
+			line = filp.readline()
+	# get a DataFrame with lost datas
+	df = pd.DataFrame({"lost":report.losts, "idx":range(1, len(report.losts)+1)})
+	
+	# fit a linear regression
+	fit = sm.ols(formula="idx ~ lost", data=df).fit()
+	predict = fit.predict(df)
+	df['fitted'] = predict
+	print df
+
+	# plot
+	fig, ax = plt.subplots(figsize=(8,4))
+	ax.scatter(df.idx, df.lost)
+	ax.plot(df.idx, df.fitted, 'r')
+	plt.show()
+
+def main():
+	for i in range(6):
+		ismemleak("final_report_mm"+str(i)+".txt")
+
+if __name__ == "__main__":
+	main()
